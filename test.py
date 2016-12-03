d1554d38b2a567ab3fea0602f152ef06b8e9e8f3
nemtiax <nemtiax@gmail.com>
2016-12-03 15:28:57 -0500

    Gotta import

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
+from generator import Generator
 
 gen = Generator("gen_1")
 
 z = tf.placeholder(tf.float32, [64, 100],name='z')
 
 gen.build(z)
-gen.build_sampler(z)                   
+gen.build_sampler(z)
