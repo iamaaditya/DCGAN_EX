4e24c673131bb3ad44041c392483a867e04ae0fe
nemtiax <nemtiax@gmail.com>
2016-12-05 14:51:43 -0500

    Added flags to test

--- test.py
+++ test.py
@@ -3,6 +3,24 @@ from generator import Generator
 from discriminator import Discriminator
 from adversarial_pair import Adversarial_Pair
 
+
+flags = tf.app.flags
+flags.DEFINE_integer("epoch", 25, "Epoch to train [25]")
+flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
+flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
+flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
+flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
+flags.DEFINE_integer("image_size", 108, "The size of image to use (will be center cropped) [108]")
+flags.DEFINE_integer("output_size", 64, "The size of the output images to produce [64]")
+flags.DEFINE_integer("c_dim", 3, "Dimension of image color. [3]")
+flags.DEFINE_string("dataset", "celebA", "The name of dataset [celebA, mnist, lsun]")
+flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
+flags.DEFINE_string("sample_dir", "samples", "Directory name to save the image samples [samples]")
+flags.DEFINE_boolean("is_train", False, "True for training, False for testing [False]")
+flags.DEFINE_boolean("is_crop", False, "True for training, False for testing [False]")
+flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothing [False]")
+FLAGS = flags.FLAGS
+
 gen = Generator("gen_1")
 disc = Discriminator("dis_1")
 
@@ -13,7 +31,7 @@ images = tf.placeholder(tf.float32, [64] + [64, 64, 3],name='real_images')
 adv = Adversarial_Pair(gen,disc)
 adv.build(z,images)
 adv.build_loss()
-adv.build_train_ops()
+adv.build_train_ops(FLAGS)
 
 
 vars = tf.all_variables()
