b1f8bba0d3fdcb11c5c4d73b28e05178dc9b119f
nemtiax <nemtiax@gmail.com>
2016-12-05 14:48:11 -0500

    AdversarialPair class and testing

---
+++ adversarial_pair.py
@@ -0,0 +1,27 @@
+class Adversarial_Pair(object):
+    def __init__(self,generator,discriminator):
+        self.generator=generator
+        self.discriminator=discriminator
+
+    def build(self,z,images,y=None):
+        self.G = self.generator.build(z,y)
+        self.D,self.D_logits = self.discriminator.build(images,y)
+        self.D_,self.D_logits_ = self.discriminator.build(self.G,y)
+        self.sampler = self.generator.build_sampler(z,y)
+
+    def build_loss(self):
+        self.d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.D_logits, tf.ones_like(self.D)))
+        self.d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.D_logits_, tf.zeros_like(self.D_)))
+        self.g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.D_logits_, tf.ones_like(self.D_)))
+
+        self.d_loss = self.d_loss_real + self.d_loss_fake
+
+    def build_train_ops(self,config):
+
+        t_vars = tf.trainable_variables()
+
+        self.d_vars = [var for var in t_vars if 'Discriminator_'+discriminator.model_name in var.name]
+        self.g_vars = [var for var in t_vars if 'Generator_'+generator.model_name in var.name]
+
+        self.d_optim = tf.train.AdamOptimizer(config.learning_rate, beta1=config.beta1).minimize(self.d_loss, var_list=self.d_vars)
+        self.g_optim = tf.train.AdamOptimizer(config.learning_rate, beta1=config.beta1).minimize(self.g_loss, var_list=self.g_vars)
--- test.py
+++ test.py
@@ -1,20 +1,19 @@
 import tensorflow as tf
 from generator import Generator
 from discriminator import Discriminator
+from adversarial_pair import Adversarial_Pair
 
 gen = Generator("gen_1")
 
 z = tf.placeholder(tf.float32, [64, 100],name='z')
 
-generated = gen.build(z)
-gen.build_sampler(z)
-
 images = tf.placeholder(tf.float32, [64] + [64, 64, 3],name='real_images')
 
-disc = Discriminator("dis_1")
+adv = Adversarial_Pair(gen,disc)
+adv.build(z,images)
+adv.build_loss()
+adv.build_train_ops()
 
-disc.build(images)
-disc.build(generated)
 
 vars = tf.all_variables()
 for var in vars:
