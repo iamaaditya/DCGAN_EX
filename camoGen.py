7bb8d8b52f77c663014510d5eb68b063837a8b6a
nemtiax <nemtiax@gmail.com>
2017-01-12 17:23:06 -0500

    bunch of stuff for running training

--- camoGen.py
+++ camoGen.py
@@ -5,7 +5,7 @@ from glob import glob
 from utils import *
 import numpy as np
 class Generator(object):
-    def __init__(self,name,batch_size=64,output_size=32):
+    def __init__(self,name,batch_size=16,output_size=32):
         self.name = name
         self.batch_size = batch_size
         self.output_size = output_size
@@ -16,18 +16,30 @@ class Generator(object):
             self.im = input_images
             self.pm = patch_masks
             self.po = patch_offsets
-            h0 = lrelu(conv2d(input_images, 8, d_h=1,d_w=1,name='g_h0_conv'))
-            h1 = lrelu(batch_norm(conv2d(h0, 32, d_h=4,d_w=4,name='g_h1_conv'),scope="g_h1_conv"))
-            h2 = lrelu(batch_norm(conv2d(h1,128,d_h=4,d_w=4,name='g_h2_conv'),scope="g_h2_conv"))
-            h3 = lrelu(batch_norm(conv2d(h2,512,d_h=4,d_w=4,name='g_h3_conv'),scope="g_h3_conv"))
-            h4,_,_ = deconv2d(h3,[self.batch_size,8,8,128],name='g_h4_deconv', with_w=True)
-            h4 = lrelu(batch_norm(h4,scope='g_h4_deconv'))
-            h5,_,_ = deconv2d(h4,[self.batch_size,16,16,16],name='g_h5_deconv',with_w=True)
-            h5 = lrelu(batch_norm(h5,scope='g_h5_deconv'))
-            h6,_,_ = deconv2d(h5,[self.batch_size,32,32,3],name='g_h6_deconv',with_w=True)
+            # h0 = lrelu(conv2d(input_images, 8, d_h=1,d_w=1,name='g_h0_conv'))
+            # h1 = lrelu(batch_norm(conv2d(h0, 32, d_h=4,d_w=4,name='g_h1_conv'),scope="g_h1_conv"))
+            # h2 = lrelu(batch_norm(conv2d(h1,128,d_h=4,d_w=4,name='g_h2_conv'),scope="g_h2_conv"))
+            # h3 = lrelu(batch_norm(conv2d(h2,512,d_h=4,d_w=4,name='g_h3_conv'),scope="g_h3_conv"))
+            # h4,_,_ = deconv2d(h3,[self.batch_size,8,8,128],name='g_h4_deconv', with_w=True)
+            # h4 = lrelu(batch_norm(h4,scope='g_h4_deconv'))
+            # h5,_,_ = deconv2d(h4,[self.batch_size,16,16,16],name='g_h5_deconv',with_w=True)
+            # h5 = lrelu(batch_norm(h5,scope='g_h5_deconv'))
+            # h6,_,_ = deconv2d(h5,[self.batch_size,32,32,3],name='g_h6_deconv',with_w=True)
 
+
+            #smaller gen
+            h0 = lrelu(conv2d(input_images, 4, d_h=2,d_w=2,name='g_h0_conv')) #128x128x4
+            h1 = lrelu(batch_norm(conv2d(h0, 16, d_h=4,d_w=4,name='g_h1_conv'),scope="g_h1_conv")) #32x32x16
+            h2 = lrelu(batch_norm(conv2d(h1,64,d_h=4,d_w=4,name='g_h2_conv'),scope="g_h2_conv")) #8x8x64
+            h3,_,_ = deconv2d(h2,[self.batch_size,16,16,32],name='g_h3_deconv', with_w=True) #16x16x32
+            h3 = lrelu(batch_norm(h3,scope='g_h3_deconv'))
+            h4,_,_ = deconv2d(h3,[self.batch_size,32,32,3],name='g_h4_deconv',with_w=True) #32x32x3
+
+
+            #minimal gen
+            # h4 = lrelu(batch_norm(conv2d(input_images,3,d_h=8,d_w=8,name='g_h0_conv'),scope='g_h0_conv'))
             #output of 32x32x3 patches
-            patches = tf.nn.tanh(h6)
+            patches = tf.nn.tanh(h4)
 
             #create hole of 0s in input images
             images_with_hole = input_images*patch_masks
@@ -49,24 +61,24 @@ class Generator(object):
             return patched_images
 
 
-with tf.Session() as sess:
-    g = Generator("gen")
-    input = tf.placeholder(tf.float32, [64,256,256,3])
-    patch_masks_placeholder = tf.placeholder(tf.float32,[64,256,256,3])
-    patch_offsets_placeholder = tf.placeholder(tf.int32,[64,2])
-    output = g.build(input,patch_masks_placeholder,patch_offsets_placeholder)
-
-    data = glob(os.path.join("./data", "texture", "*.jpg"))
-    sample_files = data[0:64]
-    sample = [get_image(sample_file, 256, is_crop=True, resize_w=256, is_grayscale = False) for sample_file in sample_files]
-    patch_masks = np.ones([64,256,256,3])
-    patch_offsets = np.zeros([64,2])
-    for i in range(0,64):
-        randX = np.random.randint(256-32)
-        randY = np.random.randint(256-32)
-        patch_masks[i,randX:randX+32,randY:randY+32,:] = 0
-        patch_offsets[i][0] = randX
-        patch_offsets[i][1] = randY
-    tf.initialize_all_variables().run()
-    patched = output.eval({g.im: sample,g.pm: patch_masks, g.po: patch_offsets})
-    save_images(patched,[8,8],"patch.png")
+# with tf.Session() as sess:
+#     g = Generator("gen")
+#     input = tf.placeholder(tf.float32, [64,256,256,3])
+#     patch_masks_placeholder = tf.placeholder(tf.float32,[64,256,256,3])
+#     patch_offsets_placeholder = tf.placeholder(tf.int32,[64,2])
+#     output = g.build(input,patch_masks_placeholder,patch_offsets_placeholder)
+#
+#     data = glob(os.path.join("./data", "texture", "*.jpg"))
+#     sample_files = data[0:64]
+#     sample = [get_image(sample_file, 256, is_crop=True, resize_w=256, is_grayscale = False) for sample_file in sample_files]
+#     patch_masks = np.ones([64,256,256,3])
+#     patch_offsets = np.zeros([64,2])
+#     for i in range(0,64):
+#         randX = np.random.randint(256-32)
+#         randY = np.random.randint(256-32)
+#         patch_masks[i,randX:randX+32,randY:randY+32,:] = 0
+#         patch_offsets[i][0] = randX
+#         patch_offsets[i][1] = randY
+#     tf.initialize_all_variables().run()
+#     patched = output.eval({g.im: sample,g.pm: patch_masks, g.po: patch_offsets})
+#     save_images(patched,[8,8],"patch.png")
--- camoPair.py
+++ camoPair.py
@@ -1,14 +1,24 @@
+import tensorflow as tf
+import os
+from ops import *
+from glob import glob
+from camoGen import Generator
+from camoDisc import Discriminator
+import time
+
 class Pair(object):
     def __init__(self, generator, discriminator):
         self.generator = generator
+        self.batch_size = generator.batch_size
         self.discriminator = discriminator
 
     def build(self):
-        self.real_images = tf.placeholder(tf.float32,[self.generator.batch_size,256,256,3],name='real_images')
-        self.patch_masks = tf.placeholder(tf.float32,[self.generator.batch_size,256,256,3],name='patch_masks')
+        self.real_images = tf.placeholder(tf.float32,[self.batch_size,256,256,3],name='real_images')
+        self.patch_masks = tf.placeholder(tf.float32,[self.batch_size,256,256,3],name='patch_masks')
+        self.patch_offsets = tf.placeholder(tf.int32,[self.batch_size,2],name='patch_offsets')
 
-        self.G = self.generator.build(self.real_images, self.patch_masks)
-        self.D, self.D_logits = self.discriminator.buil(self.real_images)
+        self.G = self.generator.build(self.real_images, self.patch_masks,self.patch_offsets)
+        self.D, self.D_logits = self.discriminator.build(self.real_images)
         self.D_,self.D_logits_ = self.discriminator.build(self.G)
 
     def build_loss(self):
@@ -21,8 +31,77 @@ class Pair(object):
     def build_train_ops(self):
         t_vars = tf.trainable_variables()
 
-        self.d_vars = [var for var in t_vars if 'Discriminator_'+self.discriminator.model_name in var.name]
-        self.g_vars = [var for var in t_vars if 'Generator_'+self.generator.model_name in var.name]
+        self.d_vars = [var for var in t_vars if 'Discriminator_'+self.discriminator.name in var.name]
+        self.g_vars = [var for var in t_vars if 'Generator_'+self.generator.name in var.name]
 
-        self.d_optim = tf.train.AdamOptimizer(config.learning_rate, beta1=config.beta1).minimize(self.d_loss, var_list=self.d_vars)
-        self.g_optim = tf.train.AdamOptimizer(config.learning_rate, beta1=config.beta1).minimize(self.g_loss, var_list=self.g_vars)
+        self.d_optim = tf.train.AdamOptimizer(0.0002, beta1=0.5).minimize(self.d_loss, var_list=self.d_vars)
+        self.g_optim = tf.train.AdamOptimizer(0.0002, beta1=0.5).minimize(self.g_loss, var_list=self.g_vars)
+
+    def load_data(self):
+        self.data = glob(os.path.join("./data", "texture", "*.jpg"))
+        self.all_images = [get_image(image, 256, is_crop=True, resize_w=256, is_grayscale = False) for image in self.data]
+        print("FINISHED LOADING DATA")
+
+    def get_batch(self,index):
+        batch = self.all_images[index*self.batch_size:(index+1)*self.batch_size]
+        batch_images = np.array(batch).astype(np.float32)
+
+        patch_masks = np.ones([self.batch_size,256,256,3])
+        patch_offsets = np.zeros([self.batch_size,2])
+        for i in range(0,self.batch_size):
+            randX = np.random.randint(256-32)
+            randY = np.random.randint(256-32)
+            patch_masks[i,randX:randX+32,randY:randY+32,:] = 0
+            patch_offsets[i][0] = randX
+            patch_offsets[i][1] = randY
+
+        return batch_images,patch_masks,patch_offsets
+
+    def train(self,epochs,sess):
+        tf.initialize_all_variables().run()
+        sample_images,sample_masks,sample_offsets = self.get_batch(0)
+        counter = 0
+        for epoch in xrange(epochs):
+            print(epoch)
+            max_index = len(self.data)//self.batch_size
+            for index in xrange(max_index):
+                start_time = time.time()
+
+                batch_images,batch_masks,batch_offsets = self.get_batch(index)
+
+                sess.run([self.d_optim],feed_dict={self.real_images: batch_images,self.patch_masks: batch_masks, self.patch_offsets: batch_offsets})
+
+                sess.run([self.g_optim],feed_dict={self.real_images: batch_images,self.patch_masks: batch_masks, self.patch_offsets: batch_offsets})
+
+                counter = counter+1
+                end_time = time.time()
+                print('{:4d}: {:.4f}'.format(index,end_time-start_time))
+                if(counter%100==0):
+                    result = self.G.eval({self.real_images: sample_images,self.patch_masks: sample_masks, self.patch_offsets: sample_offsets})
+                    save_images(result,[4,4],'./camo_samples/train_{:02d}_{:04d}_1.png'.format(epoch,index))
+                    print(counter)
+
+
+    def print_model_params(verbose=True):
+        total_parameters = 0
+        for variable in tf.trainable_variables():
+            shape = variable.get_shape()
+            if verbose: print("name: " + str(variable.name) + " - shape:" + str(shape))
+            variable_parametes = 1
+            for dim in shape:
+                variable_parametes *= dim.value
+            if verbose: print("variable parameters: " , variable_parametes)
+            total_parameters += variable_parametes
+        if verbose: print("total params: ", total_parameters)
+        return total_parameters
+
+with tf.Session() as sess:
+    g = Generator("gen")
+    d = Discriminator("disc")
+    p = Pair(g,d)
+    p.build()
+    p.build_loss()
+    p.build_train_ops()
+    p.print_model_params()
+    p.load_data()
+    p.train(100,sess)
